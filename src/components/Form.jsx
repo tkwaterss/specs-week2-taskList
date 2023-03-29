0a0ff3dc3d0eab578f36e101e3d1bba517044c25
Tobin Waters <tkwaterss@gmail.com>
2023-03-29 09:00:21 -0600

    useRef implemented for inputs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,36 @@
 import React, { useRef } from "react";
 
 const Form = (props) => {
+  const taskRef = useRef();
+  const categoryRef = useRef();
+  const dateRef = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let newTask = {
+      task: taskRef.current.value,
+      category: categoryRef.current.value,
+      date: dateRef.current.value,
+    };
+    console.log(newTask);
+
+    taskRef.current.value = "";
+    categoryRef.current.value = "";
+    dateRef.current.value = null;
+    taskRef.current.focus();
+  };
+
   return (
-    <form className="card">
+    <form className="card" onSubmit={handleSubmit}>
       <h2>Add a Task</h2>
-      <input type="text" placeholder="Task" />
-      <select>
-        <option>Pick a Category</option>
-        <option>Home</option>
-        <option>Work</option>
-        <option>Personal</option>
+      <input type="text" placeholder="Task" ref={taskRef} />
+      <select ref={categoryRef}>
+        <option value="">Pick a Category</option>
+        <option value="Home">Home</option>
+        <option value="Work">Work</option>
+        <option value="Personal">Personal</option>
       </select>
-      <input type="date" />
+      <input type="date" ref={dateRef} />
       <button>Add</button>
     </form>
   );
